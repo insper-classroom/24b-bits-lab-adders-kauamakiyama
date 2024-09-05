4aba7411a87e1aa2198906994e338d573b37f2bc
kauamakiyama <159027977+kauamakiyama@users.noreply.github.com>
2024-09-05 12:33:18 +0000

    .

--- ula_modules.py
+++ ula_modules.py
@@ -11,22 +11,37 @@ from myhdl import *
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
-        pass
+        soma.next = a ^ b
+        carry.next = a & b
 
     return instances()
 
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    s = [Signal(bool(0)) for i in range(3)]
+
+    half_1 = halfAdder(a, b, s[0], s[1]) 
+    half_2 = halfAdder(c, s[0], soma, s[2])
+
     @always_comb
     def comb():
-        pass
+        carry.next = s[1] | s[2]
 
     return instances()
 
 
 @block
 def adder2bits(x, y, soma, carry):
+    soma_intermediaria = Signal(bool(0))
+    carry_intermediario = Signal(bool(0))
+
+
+    full_1 = fullAdder(x[0], y[0], 0, soma[0], carry_intermediario)
+
+
+    full_2 = fullAdder(x[1], y[1], carry_intermediario, soma[1], carry)
+
     return instances()
 
 
